da126795769345fab70032c5e05a0264a73891d5
Li <lixueyi83@gmail.com>
2017-09-27 13:12:21 -0700

    Implemented function to write count and timestamp into file

--- main.cpp
+++ main.cpp
@@ -5,8 +5,16 @@
 	> Created Time: Wed 27 Sep 2017 12:36:13 PM PDT
  ************************************************************************/
 
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+#include <mutex>
 #include <boost/circular_buffer.hpp>
+
+
+
+
 using namespace std;
 
 
@@ -18,6 +26,7 @@ class RandomCountGenerator
         
         int generate_random_num();
         double get_frequency(int num); 
+        void write_num_to_file();
         
     private:
         boost::circular_buffer<int> m_last_num_queue;
@@ -81,6 +90,17 @@ double RandomCountGenerator::get_frequency(int num)
     }
     
     return ret;
+}
+
+void RandomCountGenerator::write_num_to_file()
+{
+    string num_time_str = "\t*** " + std::to_string(m_last_num_queue[m_last_num_queue.size()-1]);
+    
+    std::time_t timestamp_ = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::ofstream out("output.txt");
+    num_time_str = num_time_str + ": @ " + std::ctime(&timestamp_);
+    out << num_time_str;
+    out.close();
 }
 
 int main()
